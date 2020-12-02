3f7808f5cda0312f94191da467247861af52de38
chen-chens <joannachenchencom@gmail.com>
2020-12-02 17:56:45 +0800

    v.0.16-201202-新增原則圖 + 修改iframe + 加入retina.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
                 
         // 手機選單點擊錨點連結，選單消失(取消勾選)
         var menu = document.getElementById('menu-toggle');
-        var closeIcon = document.getElementById("menu-controler");
+        var closeIcon = document.getElementById('menu-controler');
 
         menu.addEventListener('click', handleMenuClick);
 
