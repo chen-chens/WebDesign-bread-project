abe497a17b3547ebd204c8261ae2fcb57478fde2
chen-chens <joannachenchencom@gmail.com>
2020-12-04 01:06:43 +0800

    v.0.17-201203-修正產品區塊 + 新增iframe + 不規則圖

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // 手機選單=========================================================================================
                 
         // 手機選單點擊錨點連結，選單消失(取消勾選)
-        var menu = document.getElementById('menu-toggle');
-        var closeIcon = document.getElementById('menu-controler');
+        const menu = document.getElementById('menu-toggle');
+        const closeIcon = document.getElementById('menu-controler');
 
         menu.addEventListener('click', handleMenuClick);
 
@@ -37,3 +37,38 @@
                signupBox.classList.add('trigger');
             } 
         });
+
+
+// 影音燈箱(目前外連無作用)=========================================================================================
+
+        // 當點選關閉鍵，撥放中的youtube停止播放(拿掉src，再重新放入)
+
+        // 寫法一: 缺點每次關閉就要重新reload
+        // const stopButton1 = document.getElementById('btn1');
+        // const stopButton2 = document.getElementById('btn2');
+        // const stopButton3 = document.getElementById('btn3');
+        // const stopButton4 = document.getElementById('btn4');
+
+        // stopButton1.onclick = function() {
+        //     var myPlayer = document.getElementById('v1'); 
+        //     myPlayer.setAttribute("src", "");
+        //     myPlayer.setAttribute("src", "https://www.youtube.com/embed/y8vLjPctrcU");
+        // }; 
+
+        // stopButton2.onclick = function() {
+        //     var myPlayer = document.getElementById('v2'); 
+        //     myPlayer.setAttribute("src", "");
+        //     myPlayer.setAttribute("src", "https://www.youtube.com/embed/EGbNI26PPYg");
+        // }; 
+
+        // stopButton3.onclick = function() {
+        //     var myPlayer = document.getElementById('v3'); 
+        //     myPlayer.setAttribute("src", "");
+        //     myPlayer.setAttribute("src", "https://www.youtube.com/embed/418KSrmpMwc");
+        // }; 
+
+        // stopButton4.onclick = function() {
+        //     var myPlayer = document.getElementById('v4'); 
+        //     myPlayer.setAttribute("src", "");
+        //     myPlayer.setAttribute("src", "https://www.youtube.com/embed/BU7ASp5TIGo");
+        // }; 
